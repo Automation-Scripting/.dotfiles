019bb584c23a0db52cc8f547830d3497272dc313
Nara Avila <nara.moraes@usp.br>
2026-05-27 10:02:50 -0300

    Update files

--- bash/modules/core/30-aliases.bash
+++ bash/modules/core/30-aliases.bash
@@ -5,9 +5,12 @@ alias ...='cd ../..'
 alias ....='cd ../../..'
 alias c='clear'
 
-reloadsh() {
-	local cwd="$PWD"
-	# shellcheck disable=SC1090
-	source ~/.bashrc
-	cd "$cwd" || return
+unalias reloadsh 2>/dev/null || true
+unalias bash 2>/dev/null || true
+
+function reloadsh {
+  local cwd="$PWD"
+  # shellcheck disable=SC1090
+  source ~/.bashrc
+  cd "$cwd" || return
 }
